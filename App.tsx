95eddf04b0ad8356dfdb8c13157a2cc0a154373d
stdev <officersurveyai@github.com>
2023-12-28 18:35:06 +0530

    Login, register and home UI

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import Login from './src/Components/Login/Login';
+import Register from './src/Components/Register/Register';
+import Home from './src/Components/Home/Home';
 
 const Drawer = createDrawerNavigator();
 
