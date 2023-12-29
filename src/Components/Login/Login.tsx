e863552582d751eb301f83b97a4177e53fdbf8ce
St Dev <st.dev321@gmail.com>
2023-12-29 21:48:41 +0530

    Update Ui

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,29 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from './LoginStyle';
 import { View, TextInput, Image, Text, Pressable, ScrollView } from 'react-native';
 
-interface Props{ 
-    navigation: any
+interface Props {
+    navigation: any;
 }
 
-const Login = ({ navigation}: Props) => {
+const Login = ({ navigation }: Props) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const isNextButtonDisabled = !email || !password;
+
+    const handleEmailChange = (text: string) => {
+        setEmail(text);
+    };
+
+    const handlePasswordChange = (text: string) => {
+        setPassword(text);
+    };
+
+    const handleNextPress = () => {
+        navigation.navigate("Home");
+    };
+
     return (
         <SafeAreaView>
+            <Pressable style={styles.backBtn} onPress={() => { navigation.goBack() }}>
+                <Image
+                    style={{ width: 25, height: 25 }}
+                    source={require("../../../assets/back.png")}
+                />
+            </Pressable>
             <ScrollView style={styles.container}>
-                <View style={styles.subContainer}>
-                    <Text style={styles.text}>Enter your Email for verification</Text>
-                    <Text style={styles.subText}>This number will be used for all ride-related communication. You shall recive an SMS with code for verification.</Text>
-                    <TextInput
-                        style={[styles.input, styles.shadowProps]}
-                        onChangeText={() => { console.log("Demo"); }}
-                        placeholder='Email'
-                    />
-                    <Pressable style={styles.buttonStyle} onPress={() => { navigation.navigate("Home") }} >
-                        <Text style={styles.buttonTextStyle}>Next</Text>
-                    </Pressable>
-                </View>
-            </ScrollView >
+                <Text style={styles.text}>Enter your Email for verification</Text>
+                <Text style={styles.subText}>This email will be used for all ride-related communication. You shall receive an SMS with a code for verification.</Text>
+                <TextInput
+                    style={[styles.input]}
+                    onChangeText={handleEmailChange}
+                    placeholder='Email'
+                    keyboardType='email-address'
+                />
+                <TextInput
+                    style={[styles.input]}
+                    onChangeText={handlePasswordChange}
+                    placeholder='Password'
+                    secureTextEntry
+                />
+                <Pressable
+                    style={[styles.buttonStyle, { opacity: isNextButtonDisabled ? 0.5 : 1 }]}
+                    onPress={handleNextPress}
+                    disabled={isNextButtonDisabled}
+                >
+                    <Text style={styles.buttonTextStyle}>Next</Text>
+                </Pressable>
+            </ScrollView>
         </SafeAreaView>
     );
 }
