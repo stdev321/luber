1a8405a5f733038a3a9fb890126908946450c806
St Dev <st.dev321@gmail.com>
2024-01-04 22:05:08 +0530

    New changes

--- src/Components/SelectPickLocation/SelectPickLocationStyle.ts
+++ src/Components/SelectPickLocation/SelectPickLocationStyle.ts
@@ -4,11 +4,24 @@ const { width, height } = Dimensions.get("window");
 
 export const styles = StyleSheet.create({
   container: {
-    marginTop: 50,
-    alignItems: "center",
+    paddingTop: 50,
+    backgroundColor:'#fff',
+    height:'100%'
+    // alignItems: "center",
   },
   demo: {
     width: width - 10
+  },
+  pickupWrap:{
+    display:'flex',
+    flexDirection:'row',
+    alignItems:'center',
+    columnGap:15,
+    paddingHorizontal:20
+  },
+  pickUpText:{
+    fontSize:17,
+    fontWeight:'600',
   },
   topSearch: {
     // flex: 1,
@@ -22,19 +35,26 @@ export const styles = StyleSheet.create({
     marginTop: 7,
     width: 30
   },
+  greenDot: {
+    width: 6,
+    height: 6,
+  },
   searchInput: {
-    width: 320,
+    width: width -25,
     // height: 50,
     justifyContent: "center",
-    alignItems: "baseline",
-    marginStart: 10,
-    marginEnd: 10,
+    alignItems: "center",
     flexDirection: "row",
     backgroundColor: "#fff",
-    padding: 7,
-    borderRadius: 10,
+    padding: 3,
+    paddingLeft:25,
+    borderRadius: 7,
     borderColor: "#d1d1d1",
-    borderWidth: 1
+    borderWidth: 1,
+    shadowColor: "#000",
+    shadowOffset: { height: -1, width: 0 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
   },
   inputBox: {
     width: 250,
@@ -42,14 +62,20 @@ export const styles = StyleSheet.create({
     borderWidth: 0,
   },
   mainContainer: {
-    marginTop: 40,
-    height: 300,
+    paddingHorizontal: 20,
+    paddingVertical: 40,
+    height: 273,
   },
   bottomContainer: {
-    width: 400,
+    // width: 400,
     flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "baseline",
+    justifyContent: "space-between",
+    alignItems: "center",
+    shadowColor: "#000",
+    // shadowOffset: { height: 0, width: 0 },
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+    borderWidth:1,
   },
   bottomButtons: {
     padding: 5,
@@ -58,7 +84,8 @@ export const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    borderColor: "#d1d1d1",
-    borderWidth: 1
-  }
+    // borderColor: "#d1d1d1",
+    // borderWidth: 1
+  },
+ 
 });
